08688dac159c17e847d1197ef98fd66904cd4b55
Ri5ux <dustinchristensen@outlook.com>
2014-12-19 17:54:58 -0600

    Fixed a problem where the facehugger overlay would render upside-down.

--- src/main/java/com/arisux/avp/event/render/FacehuggerRenderEvent.java
+++ src/main/java/com/arisux/avp/event/render/FacehuggerRenderEvent.java
@@ -1,9 +1,13 @@
 package com.arisux.avp.event.render;
 
+import static org.lwjgl.opengl.GL11.glRotatef;
+import static org.lwjgl.opengl.GL11.glTranslatef;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.Pre;
+
+import org.lwjgl.opengl.GL11;
 
 import com.arisux.airi.lib.RenderUtil;
 import com.arisux.avp.AliensVsPredator;
@@ -23,9 +27,15 @@ public class FacehuggerRenderEvent
 		{
 			if (event.type == RenderGameOverlayEvent.ElementType.HOTBAR)
 			{
-				if (mc.gameSettings.thirdPersonView == 0 && mc.inGameHasFocus && mc.thePlayer.riddenByEntity != null && mc.thePlayer.riddenByEntity instanceof EntityFacehugger)
+				if (mc.gameSettings.thirdPersonView == 0 && mc.thePlayer.riddenByEntity != null && mc.thePlayer.riddenByEntity instanceof EntityFacehugger)
 				{
-					RenderUtil.renderOverlay(resOverlayFacehugger);
+					GL11.glPushMatrix();
+					{
+						glRotatef(180F, 0, 0, 1);
+						glTranslatef(-RenderUtil.scaledDisplayResolution().getScaledWidth(), -RenderUtil.scaledDisplayResolution().getScaledHeight(), 0);
+						RenderUtil.renderOverlay(resOverlayFacehugger);
+					}
+					GL11.glPopMatrix();
 				}
 			}
 		}
