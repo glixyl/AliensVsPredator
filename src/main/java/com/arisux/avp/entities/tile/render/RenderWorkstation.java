9b08db70fc4ec4fd2459c7f1d2297dc981c15aac
Ri5ux <dustinchristensen@outlook.com>
2015-03-11 05:50:17 -0500

    Updated build.gradle

--- src/main/java/com/arisux/avp/entities/tile/render/RenderWorkstation.java
+++ src/main/java/com/arisux/avp/entities/tile/render/RenderWorkstation.java
@@ -46,59 +46,11 @@ public class RenderWorkstation extends TileEntitySpecialRenderer
 				this.bindTexture(AliensVsPredator.resources().WORKSTATION_MASK);
 				this.model.render(tile, 0.0625F);
 
-				GL11.glPushMatrix();
-				{
-					float textscale = 0.004F;
-					GL11.glTranslatef(0.45F, -0.58F, 0.2551F);
-					GL11.glRotatef(5F, 1F, 0F, 0F);
-					GL11.glScalef(-textscale, textscale, textscale);
+				this.renderDisplay();
 
-					int lines = 11;
-					String[] displayText = new String[lines];
-
-					{
-						displayText[0] = "\u00A74AsusTek";
-						if (Minecraft.getMinecraft().thePlayer.worldObj.getWorldTime() % 6 == 0)
-						{
-							displayText[0] = displayText[0] + "_";
-						}
-
-						displayText[4] = "     Press CTRL + ALT + DELETE to unlock.";
-						displayText[5] = "                 User is logged on.";
-						displayText[7] = "                     \u00A73Switch User";
-						displayText[9] = "             Windows Server 2008 R2";
-						displayText[10] = "______________________________________";
-					}
-
-					{
-						try
-						{
-							displayText = new String[lines];
-							int linestart = 0;
-							displayText[linestart++] = "OS: \u00A77" + SystemUtil.osName() + " (" + SystemUtil.osVersion() + ") " + SystemUtil.osArchitecture();
-							displayText[linestart++] = "User:  \u00A77" + SystemUtils.USER_NAME;
-							displayText[linestart++] = "Country:  \u00A77" + SystemUtils.USER_COUNTRY;
-							displayText[linestart++] = "Language:  \u00A77" + SystemUtils.USER_LANGUAGE;
-							displayText[linestart++] = "Java:  \u00A77" + SystemUtil.javaVersion();
-							displayText[linestart++] = "MAC:  \u00A77" + "";
-							displayText[linestart++] = "CPU Cores:  \u00A77" + SystemUtil.cpuCores();
-							displayText[linestart++] = "GPU:  \u00A77" + SystemUtil.gpu();
-							displayText[linestart++] = "GPU Vendor:  \u00A77" + (SystemUtil.gpuVendor().contains("NVIDIA") ? "\u00A7a" : (SystemUtil.gpuVendor().contains("AMD") || (SystemUtil.gpuVendor()).contains("ATI") ? "\u00A7c" : "\u00A7b")) + SystemUtil.gpuVendor();
-							displayText[linestart++] = "VMRAM:  \u00A77" + (SystemUtil.toMBFromB(SystemUtil.vmMemoryTotalBytes()) - SystemUtil.toMBFromB(SystemUtil.vmMemoryFreeBytes())) + "MB/" + SystemUtil.toMBFromB(SystemUtil.vmMemoryTotalBytes()) + "MB";
-						}
-						catch (Exception e)
-						{
-							;
-						}
-					}
-
-					for (int l = 0; l < lines - 2; l++)
-					{
-						RenderUtil.drawString(String.format("%s", displayText[l] == null ? "" : displayText[l]), 0, l * 10, 0xFFFFFFFF);
-						RenderUtil.drawProgressBar(displayText[9], (int)SystemUtil.toMBFromB(SystemUtil.vmMemoryTotalBytes()), (int)(SystemUtil.toMBFromB(SystemUtil.vmMemoryTotalBytes()) - SystemUtil.toMBFromB(SystemUtil.vmMemoryFreeBytes())), 0, 100, 220, 1, 0, 0xFFFFAA00, false);
-					}
-				}
-				GL11.glPopMatrix();
+				GL11.glRotatef(19.15F, 0F, 1F, 0F);
+				GL11.glTranslatef(0.88F, 0F, 0.149F);
+				this.renderDisplay();
 
 				glDisable(GL_BLEND);
 				RenderUtil.glEnableLight();
@@ -106,5 +58,62 @@ public class RenderWorkstation extends TileEntitySpecialRenderer
 			}
 		}
 		glPopMatrix();
+	}
+	
+	public void renderDisplay()
+	{
+		GL11.glPushMatrix();
+		{
+			float textscale = 0.004F;
+			GL11.glTranslatef(0.45F, -0.58F, 0.254F);
+			GL11.glRotatef(6.5F, 1F, 0F, 0F);
+			GL11.glScalef(-textscale, textscale, textscale);
+
+			int lines = 11;
+			String[] displayText = new String[lines];
+
+			{
+				displayText[0] = "\u00A74AsusTek";
+				if (Minecraft.getMinecraft().thePlayer.worldObj.getWorldTime() % 6 == 0)
+				{
+					displayText[0] = displayText[0] + "_";
+				}
+
+				displayText[4] = "     Press CTRL + ALT + DELETE to unlock.";
+				displayText[5] = "                 User is logged on.";
+				displayText[7] = "                     \u00A73Switch User";
+				displayText[9] = "             Windows Server 2008 R2";
+				displayText[10] = "______________________________________";
+			}
+
+			{
+				try
+				{
+					displayText = new String[lines];
+					int linestart = 0;
+					displayText[linestart++] = "OS: \u00A77" + SystemUtil.osName() + " (" + SystemUtil.osVersion() + ") " + SystemUtil.osArchitecture();
+					displayText[linestart++] = "User:  \u00A77" + SystemUtils.USER_NAME;
+					displayText[linestart++] = "Country:  \u00A77" + SystemUtils.USER_COUNTRY;
+					displayText[linestart++] = "Language:  \u00A77" + SystemUtils.USER_LANGUAGE;
+					displayText[linestart++] = "Java:  \u00A77" + SystemUtil.javaVersion();
+					displayText[linestart++] = "MAC:  \u00A77" + "";
+					displayText[linestart++] = "CPU Cores:  \u00A77" + SystemUtil.cpuCores();
+					displayText[linestart++] = "GPU:  \u00A77" + SystemUtil.gpu();
+					displayText[linestart++] = "GPU Vendor:  \u00A77" + (SystemUtil.gpuVendor().contains("NVIDIA") ? "\u00A7a" : (SystemUtil.gpuVendor().contains("AMD") || (SystemUtil.gpuVendor()).contains("ATI") ? "\u00A7c" : "\u00A7b")) + SystemUtil.gpuVendor();
+					displayText[linestart++] = "VMRAM:  \u00A77" + (SystemUtil.toMBFromB(SystemUtil.vmMemoryTotalBytes()) - SystemUtil.toMBFromB(SystemUtil.vmMemoryFreeBytes())) + "MB/" + SystemUtil.toMBFromB(SystemUtil.vmMemoryTotalBytes()) + "MB";
+				}
+				catch (Exception e)
+				{
+					;
+				}
+			}
+
+			for (int l = 0; l < lines - 2; l++)
+			{
+				RenderUtil.drawString(String.format("%s", displayText[l] == null ? "" : displayText[l]), 0, l * 10, 0xFFFFFFFF);
+				RenderUtil.drawProgressBar(displayText[9], (int)SystemUtil.toMBFromB(SystemUtil.vmMemoryTotalBytes()), (int)(SystemUtil.toMBFromB(SystemUtil.vmMemoryTotalBytes()) - SystemUtil.toMBFromB(SystemUtil.vmMemoryFreeBytes())), 0, 100, 220, 1, 0, 0xFFFFAA00, false);
+			}
+		}
+		GL11.glPopMatrix();
 	}
 }
