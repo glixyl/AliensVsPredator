ddadf201cc4fa95399aabfa0562772b610865248
root <root@A101170>
2015-03-19 09:23:16 -0500

    Removed an extra, unused variable.

--- src/main/java/com/arisux/avp/AliensVsPredator.java
+++ src/main/java/com/arisux/avp/AliensVsPredator.java
@@ -13,134 +13,133 @@ import cpw.mods.fml.common.event.*;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-@Mod(modid=AliensVsPredator.ID)
+@Mod(modid = AliensVsPredator.ID)
 public class AliensVsPredator implements IMod
 {
 	protected static final String ID = "avp";
-	
+
 	@Mod.Instance(AliensVsPredator.ID)
 	private static AliensVsPredator instance;
 	private ModContainer container;
-	public CreativeTab tab;
 	public ItemHandler items;
 	public BlockHandler blocks;
 	public Updater updater;
-	
+
 	public static AliensVsPredator instance()
 	{
 		return AliensVsPredator.instance;
 	}
-	
+
 	public static ItemHandler items()
 	{
 		return instance().items == null ? instance().items = new ItemHandler() : instance().items;
 	}
-	
+
 	public static BlockHandler blocks()
 	{
 		return instance().blocks == null ? instance().blocks = new BlockHandler() : instance().blocks;
 	}
-	
+
 	public static LocalEventHandler events()
 	{
 		return LocalEventHandler.instance;
 	}
-	
+
 	public static NetworkHandler network()
 	{
 		return NetworkHandler.instance;
 	}
-	
+
 	public static DimensionHandler dimensions()
 	{
 		return DimensionHandler.instance;
 	}
-	
+
 	public static WorldGenHandler worldgen()
 	{
 		return WorldGenHandler.instance;
 	}
-	
+
 	public static EntityHandler entities()
 	{
 		return EntityHandler.instance;
 	}
-	
+
 	@SideOnly(Side.CLIENT)
 	public static RenderingHandler renderer()
 	{
 		return RenderingHandler.instance;
 	}
-	
+
 	@SideOnly(Side.CLIENT)
 	public static KeybindHandler keybinds()
 	{
 		return KeybindHandler.instance;
 	}
-	
+
 	@SideOnly(Side.CLIENT)
 	public static Resources resources()
 	{
 		return Resources.instance;
 	}
-	
+
 	public static GuiHandler interfaces()
 	{
 		return GuiHandler.instance;
 	}
-	
+
 	public static Properties properties()
 	{
 		return Properties.instance;
 	}
-	
+
 	public static CraftingHandler crafting()
 	{
 		return CraftingHandler.instance;
 	}
-	
+
 	public static PlayerModeHandler playermodehandler()
 	{
 		return PlayerModeHandler.instance;
 	}
-	
+
 	public static AssemblerAPI assembler()
 	{
 		return AssemblerAPI.instance;
 	}
-	
+
 	public static WristbracerAPI wristbracer()
 	{
 		return WristbracerAPI.instance;
 	}
-	
+
 	public static Schematics schematics()
 	{
 		return Schematics.instance;
 	}
-	
+
 	public static CommandHandler commands()
 	{
 		return CommandHandler.instance;
 	}
-	
+
 	public static Settings settings()
 	{
 		return Settings.instance;
 	}
-	
+
 	@Override
 	public ModContainer container()
 	{
 		return this.container == null ? this.container = ModUtil.getModContainerForId(AliensVsPredator.ID) : this.container;
 	}
-	
+
 	@Override
 	public String domain()
 	{
 		return container().getModId() + ":";
 	}
-	
+
 	@Override
 	public CreativeTab tab()
 	{
@@ -153,7 +152,7 @@ public class AliensVsPredator implements IMod
 	{
 		AIRI.logger.info("[AliensVsPredator] Copyright(C) 2012-2015 Arisux");
 		AIRI.logger.info("[AliensVsPredator] Pre-Initialization");
-		
+
 		settings().preInitialize(event);
 
 		if (FMLCommonHandler.instance().getSide() == Side.CLIENT)
