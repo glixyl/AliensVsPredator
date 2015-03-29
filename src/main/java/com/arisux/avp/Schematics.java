b158ed781373098524063838ee13346bf85ad049
Dustin Christensen <Ri5ux@users.noreply.github.com>
2015-03-29 07:38:33 -0500

    Finalized Java 6 compatibility. Finished the basic schematic structure loading and generation feature. Added a new test structure. Removed previous test structure.

--- src/main/java/com/arisux/avp/Schematics.java
+++ src/main/java/com/arisux/avp/Schematics.java
@@ -3,8 +3,8 @@ package com.arisux.avp;
 import java.io.File;
 
 import com.arisux.airi.lib.interfaces.IInitializable;
-import com.arisux.airi.lib.world.*;
-import com.arisux.airi.lib.world.Schematic.UnsupportedSchematicFormatException;
+import com.arisux.airi.lib.world.Schematic;
+import com.arisux.airi.lib.world.SchematicLoader;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
@@ -18,19 +18,12 @@ public class Schematics implements IInitializable
 	@Override
 	public void initialize(FMLInitializationEvent event)
 	{
-		try
+		if (!baseSchematicDir.exists())
 		{
-			if (!baseSchematicDir.exists())
-			{
-				baseSchematicDir.mkdirs();
-			}
-			
-			this.schematicTest = SchematicLoader.extractAndLoadSchematic(new File(baseSchematicDir, "test.schematic"), AliensVsPredator.class.getResourceAsStream("/assets/avp/schematics/test.schematic"));
-			this.derelict = SchematicLoader.extractAndLoadSchematic(new File(baseSchematicDir, "derelict.schematic"), AliensVsPredator.class.getResourceAsStream("/assets/avp/schematics/derelict.schematic"));
+			baseSchematicDir.mkdirs();
 		}
-		catch (UnsupportedSchematicFormatException e)
-		{
-			e.printStackTrace();
-		}
+
+		this.schematicTest = SchematicLoader.loadSchematic(new File(baseSchematicDir, "test.schematic"), AliensVsPredator.class.getResource("/assets/avp/schematics/test.schematic"));
+		// this.derelict = SchematicLoader.extractAndLoadSchematic(new File(baseSchematicDir, "derelict.schematic"), AliensVsPredator.class.getResource("/assets/avp/schematics/derelict.schematic"));
 	}
 }
