4e78cec0fc10a85c30a03280e0d050eeae801990
Dustin Christensen <Ri5ux@users.noreply.github.com>
2015-07-19 19:01:11 -0500

    Added renderers for a few tile entities.

---
+++ src/main/java/com/arisux/avp/entities/tile/render/RenderR2PConverter.java
@@ -0,0 +1,37 @@
+package com.arisux.avp.entities.tile.render;
+
+import static org.lwjgl.opengl.GL11.glPopMatrix;
+import static org.lwjgl.opengl.GL11.glPushMatrix;
+import static org.lwjgl.opengl.GL11.glTranslated;
+
+import org.lwjgl.opengl.GL11;
+
+import com.arisux.airi.lib.RenderUtil;
+
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.tileentity.TileEntity;
+
+public class RenderR2PConverter extends TileEntitySpecialRenderer
+{
+	@Override
+	public void renderTileEntityAt(TileEntity tileEntity, double posX, double posY, double posZ, float renderPartialTicks)
+	{
+		glPushMatrix();
+		{
+			GL11.glDisable(GL11.GL_CULL_FACE);
+			glTranslated(posX, posY, posZ);
+			RenderUtil.drawRect(0, 0, 1, 1, 0xFFFF0000);
+			GL11.glTranslated(0, 0, 1);
+			RenderUtil.drawRect(0, 0, 1, 1, 0xFFFF0000);
+			GL11.glRotatef(90, 1, 0, 0);
+			RenderUtil.drawRect(0, 0, 1, -1, 0xFFFF0000);
+			GL11.glTranslated(0, 0, -1);
+			RenderUtil.drawRect(0, 0, 1, -1, 0xFFFF0000);
+			GL11.glRotatef(90, 0, 1, 0);
+			RenderUtil.drawRect(0, 0, -1, -1, 0xFFFF0000);
+			GL11.glTranslated(0, 0, 1);
+			RenderUtil.drawRect(0, 0, -1, -1, 0xFFFF0000);
+		}
+		glPopMatrix();
+	}
+}
---
+++ src/main/java/com/arisux/avp/items/render/RenderItemPowerline.java
@@ -0,0 +1,66 @@
+package com.arisux.avp.items.render;
+
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
+
+import org.lwjgl.opengl.GL11;
+
+import com.arisux.airi.lib.RenderUtil;
+import com.arisux.airi.lib.client.ItemRenderer;
+import com.arisux.airi.lib.client.ModelBaseExtension;
+import com.arisux.avp.entities.tile.model.ModelCable;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
+
+public class RenderItemPowerline extends ItemRenderer
+{
+	public static final ModelBaseExtension model = new ModelCable();
+	private float rotation;
+	
+	public RenderItemPowerline()
+	{
+		super(model, null);
+	}
+	
+	@Override
+	public void renderItem(ItemRenderType type, ItemStack item, Object... data)
+	{
+		this.rotation = rotation > 360F ? rotation = 0F : (rotation = rotation + 0.6F);
+		super.renderItem(type, item, data);
+	}
+
+	@Override
+	public void renderThirdPerson(ItemStack item, Object... data)
+	{
+		;
+	}
+
+	@Override
+	public void renderFirstPerson(ItemStack item, Object... data)
+	{
+		GL11.glPushMatrix();
+		{
+
+		}
+		GL11.glPopMatrix();
+	}
+
+	@Override
+	public void renderInInventory(ItemStack item, Object... data)
+	{
+		GL11.glPushMatrix();
+		{
+			float glScale = 12F;
+			glDisable(GL11.GL_TEXTURE_2D);
+			GL11.glScalef(glScale, glScale, glScale);
+			GL11.glTranslated(1.1, -0.1, 0);
+			GL11.glRotatef(45, 1, 0, 1);
+			GL11.glRotatef(Minecraft.getMinecraft().thePlayer.worldObj.getWorldTime() % 360 * 8, 0, 1, 0);
+			RenderUtil.glColorHexRGBA(0xFF222222);
+			model.render(0.0625F);
+			glEnable(GL11.GL_TEXTURE_2D);
+		}
+		GL11.glPopMatrix();
+	}
+}
