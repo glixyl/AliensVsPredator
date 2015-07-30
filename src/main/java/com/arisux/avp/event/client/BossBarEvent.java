0d2742701fe48e8caca4d665e4ab2e2954639f8b
Dustin Christensen <Ri5ux@users.noreply.github.com>
2015-07-30 17:24:10 -0500

    Boss bars are now horizontally aligned. Boss bars are now functional. Boss bar texture has been sligtly modified.

--- src/main/java/com/arisux/avp/event/client/BossBarEvent.java
+++ src/main/java/com/arisux/avp/event/client/BossBarEvent.java
@@ -10,15 +10,15 @@ import org.lwjgl.opengl.GL11;
 import com.arisux.airi.lib.RenderUtil;
 import com.arisux.airi.lib.WorldUtil;
 import com.arisux.airi.lib.WorldUtil.Blocks.CoordData;
-import com.arisux.airi.lib.client.ScaledResolution;
 import com.arisux.avp.AliensVsPredator;
 import com.arisux.avp.entities.mob.EntityQueen;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
-import cpw.mods.fml.common.gameevent.TickEvent.RenderTickEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 
 public class BossBarEvent
 {
@@ -35,12 +35,19 @@ public class BossBarEvent
 	{
 		if (mc.thePlayer != null)
 		{
+			ArrayList<EntityLivingBase> bossesToRemove = new ArrayList<EntityLivingBase>();
+
 			for (EntityLivingBase boss : bosses)
 			{
 				if (boss.isDead)
 				{
-					this.bosses.remove(boss);
+					bossesToRemove.add(boss);
 				}
+			}
+
+			for (EntityLivingBase boss : bossesToRemove)
+			{
+				this.bosses.remove(boss);
 			}
 
 			if (mc.thePlayer.worldObj.getWorldTime() % 40 == 0)
@@ -60,38 +67,43 @@ public class BossBarEvent
 	}
 
 	@SubscribeEvent
-	public void renderTick(RenderTickEvent event)
+	public void renderTick(RenderGameOverlayEvent event)
 	{
-		ScaledResolution res = RenderUtil.scaledDisplayResolution();
-
-		GL11.glPushMatrix();
+		if (event.type == ElementType.BOSSHEALTH)
 		{
-			float scale = 0.5F;
-			GL11.glScalef(scale, scale, scale);
-			
-			for (EntityLivingBase boss : bosses)
+			GL11.glPushMatrix();
 			{
-				int index = bosses.indexOf(boss);
-				this.drawBossBar(boss.getCommandSenderName(), index, res.getScaledWidth(), 0);
+				float scale = 0.5F;
+				GL11.glScalef(scale, scale, scale);
+
+				for (EntityLivingBase boss : bosses)
+				{
+					int index = bosses.indexOf(boss);
+					this.drawBossBar(boss, index, 0, 0);
+				}
 			}
+			GL11.glPopMatrix();
 		}
-		GL11.glPopMatrix();
 	}
 
-	public void drawBossBar(String label, int index, int posX, int posY)
+	public void drawBossBar(EntityLivingBase boss, int index, int posX, int posY)
 	{
+		String label = boss.getCommandSenderName();
+		int tW = 233;
+		int tH = 50;
+		int offset = tW * 30 / 100;
+		int health = (int) (boss.getHealth() * 100 / boss.getMaxHealth());
+
 		GL11.glPushMatrix();
 		{
 			GL11.glEnable(GL11.GL_BLEND);
 			GL11.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 			RenderUtil.glColorHexRGBA(0xFFFFFFFF);
 			RenderUtil.bindTexture(AliensVsPredator.resources().QUEEN_BOSS_BAR);
-			int tW = 233;
-			int tH = 50;
-			posY = posY + (index * tH);
-			RenderUtil.drawQuad(posX - (tW / 2), posY, tW, tH, 0, 0F, 1F, 0F, 0.5F);
-			RenderUtil.drawQuad(posX - (tW / 2), posY, tW, tH, 0, 0F, 1F, 0.5F, 1F);
-			RenderUtil.drawStringAlignCenter(label, posX, posY + 16, 0xFF00FF00);
+			posX = posX + (index * (tW));
+			RenderUtil.drawQuad(posX + (offset / 2), posY, (tW - offset) * health / 100, tH, 0, 0.15F, 0.85F, 0F, 0.5F);
+			RenderUtil.drawQuad(posX, posY, tW, tH, 0, 0F, 1F, 0.5F, 1F);
+			RenderUtil.drawStringAlignCenter(label, posX + (tW / 2), posY + 16, 0xFF00FF00);
 			GL11.glDisable(GL11.GL_BLEND);
 		}
 		GL11.glPopMatrix();
