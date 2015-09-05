36191f627841e599056b1c66a35b84f9819ca275
HassanS6000 <hassansyyid@gmail.com>
2015-09-04 21:47:09 -0400

    Ampule Now Faces You On Placement.

--- src/main/java/com/arisux/avp/entities/tile/TileEntityAmpule.java
+++ src/main/java/com/arisux/avp/entities/tile/TileEntityAmpule.java
@@ -1,25 +1,66 @@
 package com.arisux.avp.entities.tile;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class TileEntityAmpule extends TileEntity
 {
+	private ForgeDirection direction;
+	
 	@Override
 	public void updateEntity()
 	{
 		;
 	}
 	
+
 	@Override
-	public void readFromNBT(NBTTagCompound tag)
+	public Packet getDescriptionPacket()
 	{
-		super.readFromNBT(tag);
+		NBTTagCompound nbtTag = new NBTTagCompound();
+		this.writeToNBT(nbtTag);
+		return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 1, nbtTag);
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity packet)
+	{
+		readFromNBT(packet.func_148857_g());
+	}
+
+	@Override
+	public void writeToNBT(NBTTagCompound nbt)
+	{
+		super.writeToNBT(nbt);
+
+		if (this.direction != null)
+		{
+			nbt.setInteger("Direction", this.direction.ordinal());
+		}
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound nbt)
+	{
+		super.readFromNBT(nbt);
+
+		if (ForgeDirection.getOrientation(nbt.getInteger("Direction")) != null)
+		{
+			this.direction = ForgeDirection.getOrientation(nbt.getInteger("Direction"));
+		}
 	}
 	
-	@Override
-	public void writeToNBT(NBTTagCompound tag)
+	public ForgeDirection getDirection()
 	{
-		super.writeToNBT(tag);
+		return direction;
+	}
+
+	public void setDirection(ForgeDirection direction)
+	{
+		this.direction = direction;
 	}
 }
