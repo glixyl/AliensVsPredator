7ed70ee8938c1d195344c35769cb5d110832810b
HassanS6000 <hassansyyid@gmail.com>
2015-09-23 14:09:26 -0400

    tada

--- src/main/java/com/arisux/avp/entities/mob/EntityXenomorph.java
+++ src/main/java/com/arisux/avp/entities/mob/EntityXenomorph.java
@@ -19,111 +19,136 @@ import net.minecraft.world.World;
 
 public abstract class EntityXenomorph extends EntitySpeciesAlien implements IMob
 {
-	public int targetQueenId;
-	protected boolean canClimb;
-	protected boolean isDependant;
+    public int targetQueenId;
+    protected boolean canClimb;
+    protected boolean isDependant;
 
-	public EntityXenomorph(World world)
-	{
-		super(world);
-		this.jumpMovementFactor = 0.06F;
-		this.canClimb = true;
-		this.isDependant = true;
-		this.getNavigator().setCanSwim(true);
-		this.tasks.addTask(0, new EntityAIQueenIdentificationTask(this));
-		this.tasks.addTask(1, new EntityAIClimb(this, 0.03F));
-		this.tasks.addTask(2, new EntityAIWander(this, 0.8D));
-		this.targetTasks.addTask(0, new EntityAIHurtByTarget(this, true));
-		this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, Entity.class, /** targetChance **/0, /** shouldCheckSight **/false, /** nearbyOnly **/false, EntitySelectorXenomorph.instance));
-		this.targetTasks.addTask(2, new EntityAIAttackOnCollide(this, 0.8D, true));
-	}
+    public EntityXenomorph(World world)
+    {
+        super(world);
+        this.jumpMovementFactor = 0.02F;
+        this.canClimb = true;
+        this.isDependant = true;
+        this.getNavigator().setCanSwim(true);
+        this.tasks.addTask(0, new EntityAIQueenIdentificationTask(this));
+        this.tasks.addTask(1, new EntityAIClimb(this, 0.03F));
+        this.tasks.addTask(2, new EntityAIWander(this, 0.8D));
+        this.targetTasks.addTask(0, new EntityAIHurtByTarget(this, true));
+        this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, Entity.class, /** targetChance **/0, /** shouldCheckSight **/false, /** nearbyOnly **/false, EntitySelectorXenomorph.instance));
+        this.targetTasks.addTask(2, new EntityAIAttackOnCollide(this, 0.8D, true));
+    }
 
-	@Override
-	protected void applyEntityAttributes()
-	{
-		super.applyEntityAttributes();
-		this.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(1F);
-		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(32.0D);
-	}
+    @Override
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(1F);
+        this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(32.0D);
+    }
 
-	@Override
-	protected void entityInit()
-	{
-		super.entityInit();
-	}
+    @Override
+    protected void entityInit()
+    {
+        super.entityInit();
+    }
 
-	@Override
-	protected boolean canDespawn()
-	{
-		return false;
-	}
+    @Override
+    protected boolean canDespawn()
+    {
+        return false;
+    }
 
-	@Override
-	protected boolean isAIEnabled()
-	{
-		return true;
-	}
+    @Override
+    protected boolean isAIEnabled()
+    {
+        return true;
+    }
 
-	public boolean canClimb()
-	{
-		return this.canClimb;
-	}
+    public boolean canClimb()
+    {
+        return this.canClimb;
+    }
 
-	@Override
-	public void onUpdate()
-	{
-		super.onUpdate();
-		this.fallDistance = 0F;
-		
-		
-	}
-	
-	@Override
-	protected void attackEntity(Entity entity, float damage)
-	{
-		super.attackEntity(entity, damage);
-	}
+    @Override
+    public void onUpdate()
+    {
+        super.onUpdate();
+        this.fallDistance = 0F;
+        
+        if(this.getAttackTarget() != null && this.getAttackTarget().isDead)
+        {
+            this.setAttackTarget(null);
+        }
+        
+        this.attackAI();
+        
+        if(this.getAttackTarget() != null)
+        {   
+            if(this.getDistanceToEntity(this.getAttackTarget()) <= 3)
+            {
+                this.attackEntityAsMob(this.getAttackTarget());
+            }
+        }
+    }
 
-	@Override
-	public boolean attackEntityAsMob(Entity entity)
-	{
-		return super.attackEntityAsMob(entity);
-	}
+    @Override
+    protected void attackEntity(Entity entity, float damage)
+    {
+        super.attackEntity(entity, damage);
+    }
 
-	public void attackAI()
-	{
-		if (worldObj.getWorldInfo().getWorldTime() % 70 == 0)
-		{
-			double range = this.getEntityAttribute(SharedMonsterAttributes.followRange).getAttributeValue();
-			Entity targetEntity = (this.worldObj.findNearestEntityWithinAABB(EntityLiving.class, this.boundingBox.expand(range * 10, 64.0D, range * 10), this));
-			Entity targetPlayer = (this.worldObj.findNearestEntityWithinAABB(EntityPlayer.class, this.boundingBox.expand(range * 10, 64.0D, range * 10), this));
-			if (targetPlayer != null && !((EntityPlayer) targetPlayer).capabilities.isCreativeMode)
-			{
-				this.setAttackTarget((EntityLivingBase) targetPlayer);
-				this.getNavigator().tryMoveToEntityLiving(targetPlayer, this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).getAttributeValue() * 2.5D);
-				if (this.isCollidedHorizontally)
-				{
-					this.addVelocity(0, 0.6D, 0);
-				}
-			}
-			else if (targetEntity != null && !(targetEntity instanceof EntityAcidPool) && !(targetEntity instanceof EntitySpeciesAlien))
-			{
-				this.setAttackTarget((EntityLivingBase) targetEntity);
-				this.getNavigator().tryMoveToEntityLiving(targetEntity, this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).getAttributeValue() * 2.5D);
-				if (this.isCollidedHorizontally)
-				{
-					this.addVelocity(0, 0.6D, 0);
-				}
-			}
-			else
-			{
-				this.setAttackTarget(null);
-			}
-		}
-	}
+    @Override
+    public boolean attackEntityAsMob(Entity entity)
+    {
+        return super.attackEntityAsMob(entity);
+    }
 
-	public boolean isDependant()
-	{
-		return this.isDependant;
-	}
+    public void attackAI()
+    {
+        if(this.getAttackTarget() != null)
+        {
+            Entity targetEntity = this.getAttackTarget();
+            
+            if (targetEntity != null && !(targetEntity instanceof EntityAcidPool) && !(targetEntity instanceof EntitySpeciesAlien))
+            {
+                if(targetEntity instanceof EntityPlayer)
+                {
+                    EntityPlayer player = (EntityPlayer) targetEntity;
+                    
+                    if(!(player.capabilities.isCreativeMode))
+                    {
+                        this.setAttackTarget(player);
+                        this.getNavigator().tryMoveToEntityLiving(targetEntity, this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).getAttributeValue() * 2.5D);
+                    }
+                    else
+                    {
+                        this.setAttackTarget(null);
+                    }
+                }
+                else
+                {
+                    this.setAttackTarget((EntityLivingBase) targetEntity);
+                    this.getNavigator().tryMoveToEntityLiving(targetEntity, this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).getAttributeValue() * 2.5D);
+                }
+            }
+            else
+            {
+                this.setAttackTarget(null);
+            }
+        }
+        else
+        {
+            if (worldObj.getWorldInfo().getWorldTime() % 70 == 0)
+            {
+                double range = this.getEntityAttribute(SharedMonsterAttributes.followRange).getAttributeValue();
+                this.setAttackTarget((EntityLivingBase) (this.worldObj.findNearestEntityWithinAABB(EntityLiving.class, this.boundingBox.expand(range * 10, 64.0D, range * 10), this)));
+                this.setAttackTarget((EntityPlayer) (this.worldObj.findNearestEntityWithinAABB(EntityPlayer.class, this.boundingBox.expand(range * 10, 64.0D, range * 10), this)));
+            }
+        }
+    }
+
+    public boolean isDependant()
+    {
+        return this.isDependant;
+    }
 }
