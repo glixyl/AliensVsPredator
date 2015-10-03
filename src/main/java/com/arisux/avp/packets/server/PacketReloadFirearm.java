dbaaa6a85835a56b94a7aa0bf6016aa38d5c7bed
Dustin Christensen <Ri5ux@users.noreply.github.com>
2015-10-02 19:21:40 -0500

    Fixed a few possible NullPointerExceptions in PacketReloadFirearm

--- src/main/java/com/arisux/avp/packets/server/PacketReloadFirearm.java
+++ src/main/java/com/arisux/avp/packets/server/PacketReloadFirearm.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.EntityPlayer;
 
 public class PacketReloadFirearm implements IMessage, IMessageHandler<PacketReloadFirearm, PacketReloadFirearm>
 {
@@ -14,20 +15,28 @@ public class PacketReloadFirearm implements IMessage, IMessageHandler<PacketRelo
 		;
 	}
 
-	@Override public void fromBytes(ByteBuf buf)
+	@Override
+	public void fromBytes(ByteBuf buf)
 	{
 		;
 	}
 
-	@Override public void toBytes(ByteBuf buf)
+	@Override
+	public void toBytes(ByteBuf buf)
 	{
 		;
 	}
 
-	@Override public PacketReloadFirearm onMessage(PacketReloadFirearm message, MessageContext ctx)
+	@Override
+	public PacketReloadFirearm onMessage(PacketReloadFirearm message, MessageContext ctx)
 	{
-		((ItemFirearm) ctx.getServerHandler().playerEntity.inventory.getCurrentItem().getItem()).reload(ctx.getServerHandler().playerEntity);
-		
+		EntityPlayer player = ctx.getServerHandler().playerEntity;
+
+		if (player != null && player.inventory != null && player.inventory.getCurrentItem() != null)
+		{
+			((ItemFirearm) player.inventory.getCurrentItem().getItem()).reload(ctx.getServerHandler().playerEntity);
+		}
+
 		return null;
 	}
 }
