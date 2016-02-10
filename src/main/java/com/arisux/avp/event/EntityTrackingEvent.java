1f046468ba63c576ec535ebff56034ddc51107e4
Hassan Syyid <hassansyyid@gmail.com>
2016-02-10 15:59:12 -0500

    Bug Fixes
    - Guns no longer reset their bullets when the user is killed.

--- src/main/java/com/arisux/avp/event/EntityTrackingEvent.java
+++ src/main/java/com/arisux/avp/event/EntityTrackingEvent.java
@@ -1,28 +1,7 @@
 package com.arisux.avp.event;
 
-import com.arisux.avp.items.ItemFirearm;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
-
 public class EntityTrackingEvent
 {
-	@SubscribeEvent
-	public void onLivingDeath(LivingDeathEvent event)
-	{
-		if (event.entityLiving instanceof EntityPlayer)
-		{
-			EntityPlayer entityPlayer = (EntityPlayer) event.entityLiving;
-
-			for (ItemStack itemStack : entityPlayer.inventory.mainInventory)
-			{
-				if (itemStack.getItem() instanceof ItemFirearm)
-				{
-					ItemFirearm itemFirearm = (ItemFirearm) itemStack.getItem();
-					itemFirearm.setAmmoCount(0);
-				}
-			}
-		}
-	}
+	;
 }
+
