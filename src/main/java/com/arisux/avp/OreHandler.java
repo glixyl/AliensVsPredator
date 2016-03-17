67bcc9b82d30c125dd1a0cab62e764da40f40be5
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-03-17 09:32:10 -0500

    Updated the names of several ingots in the ore dictionary. Added ores to the ore dictionary.

--- src/main/java/com/arisux/avp/OreHandler.java
+++ src/main/java/com/arisux/avp/OreHandler.java
@@ -10,10 +10,15 @@ public class OreHandler implements IInitializable
 	@Override
 	public void initialize(FMLInitializationEvent event)
 	{
-		OreDictionary.registerOre("copper", AliensVsPredator.items().itemIngotCopper);
+		OreDictionary.registerOre("oreCopper", AliensVsPredator.blocks().oreCopper);
+		OreDictionary.registerOre("ingotCopper", AliensVsPredator.items().itemIngotCopper);
+		OreDictionary.registerOre("oreLithium", AliensVsPredator.blocks().oreLithium);
 		OreDictionary.registerOre("lithium", AliensVsPredator.items().itemIngotLithium);
-		OreDictionary.registerOre("aluminum", AliensVsPredator.items().itemIngotAluminum);
-		OreDictionary.registerOre("aluminium", AliensVsPredator.items().itemIngotAluminum);
+		OreDictionary.registerOre("oreAluminum", AliensVsPredator.blocks().oreBauxite);
+		OreDictionary.registerOre("ingotAluminum", AliensVsPredator.items().itemIngotAluminum);
+		OreDictionary.registerOre("oreAluminium", AliensVsPredator.blocks().oreBauxite);
+		OreDictionary.registerOre("ingotAluminium", AliensVsPredator.items().itemIngotAluminum);
+		OreDictionary.registerOre("oreSilicon", AliensVsPredator.blocks().oreSilicon);
 		OreDictionary.registerOre("silicon", AliensVsPredator.items().itemSilicon);
 		OreDictionary.registerOre("polycarbonate", AliensVsPredator.items().itemPolycarbonate);
 		OreDictionary.registerOre("plastic", AliensVsPredator.items().itemPolycarbonate);
