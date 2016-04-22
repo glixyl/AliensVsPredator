9f88288f9c863c60fc5fd3a5cba6c93bf6c3cac2
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-04-22 09:55:09 -0500

    Ovamorphs will now hatch if they are damaged or if they are collided with entities other than members of the alien species.

--- src/main/java/com/arisux/avp/entities/mob/EntityOvamorph.java
+++ src/main/java/com/arisux/avp/entities/mob/EntityOvamorph.java
@@ -2,8 +2,10 @@ package com.arisux.avp.entities.mob;
 
 import com.arisux.avp.AliensVsPredator;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.monster.IMob;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
@@ -11,6 +13,7 @@ public class EntityOvamorph extends EntitySpeciesAlien implements IMob
 {
 	public int hatchingTime;
 	public boolean hasHatched;
+	public boolean acceleratedHatching;
 
 	public EntityOvamorph(World par1World)
 	{
@@ -72,20 +75,46 @@ public class EntityOvamorph extends EntitySpeciesAlien implements IMob
         int y = MathHelper.floor_double(this.posY);
         int z = MathHelper.floor_double(this.posZ);
 
-		if (this.worldObj.getBlock(x, y, z).getMaterial() != AliensVsPredator.materials().mist)
+		if (this.worldObj.getBlock(x, y, z).getMaterial() != AliensVsPredator.materials().mist || this.acceleratedHatching)
 		{
+			int hatchAcceleration = this.acceleratedHatching ? 8 : 1;
+			
 			if (this.worldObj.getClosestPlayerToEntity(this, 15.0D) != null || this.hasHatched)
 			{
-				if (!this.worldObj.isRemote && this.hatchingTime-- <= 1 || this.hasHatched)
+				if (!this.worldObj.isRemote && (this.hatchingTime -= hatchAcceleration) <= 1 || this.hasHatched)
 				{
-					EntityFacehugger facehugger = new EntityFacehugger(this.worldObj);
-					facehugger.setLocationAndAngles(posX, posY, posZ, 0F, 0F);
-					worldObj.spawnEntityInWorld(facehugger);
-	
-					this.setDead();
+					this.hatch();
 				}
 			}
 		}
+	}
+	
+	@Override
+	protected void collideWithEntity(Entity entity)
+	{
+		super.collideWithEntity(entity);
+		
+		if (!(entity instanceof EntityOvamorph) && !(entity instanceof EntitySpeciesAlien))
+		{
+			this.acceleratedHatching = true;
+		}
+	}
+	
+	@Override
+	protected void damageEntity(DamageSource source, float amount)
+	{
+		super.damageEntity(source, amount);
+		
+		this.acceleratedHatching = true;
+	}
+	
+	private void hatch()
+	{
+		EntityFacehugger facehugger = new EntityFacehugger(this.worldObj);
+		facehugger.setLocationAndAngles(posX, posY, posZ, 0F, 0F);
+		worldObj.spawnEntityInWorld(facehugger);
+
+		this.setDead();
 	}
 	
 	public void setHatched(boolean hasHatched)
