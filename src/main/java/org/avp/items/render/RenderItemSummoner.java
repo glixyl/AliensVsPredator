ec74371739f8c788425af181c6b6d3091e68c67d
Ri5ux <Ri5ux@users.noreply.github.com>
2016-05-25 23:44:05 -0500

    Completely disabled the rendering of summoners in attempt to debug the still existing rendering glitches in inventories.

--- src/main/java/org/avp/items/render/RenderItemSummoner.java
+++ src/main/java/org/avp/items/render/RenderItemSummoner.java
@@ -1,20 +1,13 @@
 package org.avp.items.render;
 
-import org.avp.entities.mob.EntityChestburster;
-import org.avp.entities.mob.EntityMarine;
-import org.avp.entities.mob.EntityXenomorph;
-import org.avp.entities.mob.EntityYautja;
 import org.lwjgl.opengl.GL11;
 
 import com.arisux.airi.lib.AccessWrapper;
 import com.arisux.airi.lib.GlStateManager;
-import com.arisux.airi.lib.RenderUtil;
-import com.arisux.airi.lib.WorldUtil.Entities;
 import com.arisux.airi.lib.client.ItemRenderer;
 import com.arisux.airi.lib.client.ModelBaseWrapper;
 import com.arisux.airi.lib.client.Texture;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
@@ -27,7 +20,6 @@ public class RenderItemSummoner extends ItemRenderer
     private float                   scale;
     private float                   x;
     private float                   y;
-    private float                   rotation;
     private ModelBaseWrapper        modelCache;
     private Texture                 textureCache;
     private Entity                  entityCache;
@@ -60,8 +52,6 @@ public class RenderItemSummoner extends ItemRenderer
     @Override
     public void renderItem(ItemRenderType type, ItemStack item, Object... data)
     {
-        this.rotation = rotation > 360F ? rotation = 0F : (rotation = rotation + 0.6F);
-
         GlStateManager.disable(GL11.GL_CULL_FACE);
         super.renderItem(type, item, data);
     }
@@ -109,62 +99,62 @@ public class RenderItemSummoner extends ItemRenderer
     @Override
     public void renderThirdPerson(ItemStack item, Object... data)
     {
-        scale = 0.5F;
-        GlStateManager.rotate(195F, 1.0F, 0.0F, 0.0F);
-        GlStateManager.rotate(180F, 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate(30F, 0.0F, 0.0F, 1.0F);
-        GlStateManager.translate(-0.5F, 0F, 0F);
-        GlStateManager.scale(scale, scale, scale);
-        this.renderCachedModel();
+//        scale = 0.5F;
+//        GlStateManager.rotate(195F, 1.0F, 0.0F, 0.0F);
+//        GlStateManager.rotate(180F, 0.0F, 1.0F, 0.0F);
+//        GlStateManager.rotate(30F, 0.0F, 0.0F, 1.0F);
+//        GlStateManager.translate(-0.5F, 0F, 0F);
+//        GlStateManager.scale(scale, scale, scale);
+//        this.renderCachedModel();
     }
 
     @Override
     public void renderFirstPerson(ItemStack item, Object... data)
     {
-        GlStateManager.rotate(195F, 1.0F, 0.0F, 0.0F);
-        GlStateManager.rotate(180F, 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate(30F, 0.0F, 0.0F, 1.0F);
-        GlStateManager.translate(-25F, 0F + y, 20.85F);
-        GlStateManager.scale(scale, scale, scale);
-        this.renderCachedModel();
+//        GlStateManager.rotate(195F, 1.0F, 0.0F, 0.0F);
+//        GlStateManager.rotate(180F, 0.0F, 1.0F, 0.0F);
+//        GlStateManager.rotate(30F, 0.0F, 0.0F, 1.0F);
+//        GlStateManager.translate(-25F, 0F + y, 20.85F);
+//        GlStateManager.scale(scale, scale, scale);
+//        this.renderCachedModel();
     }
 
     @Override
     public void renderInInventory(ItemStack item, Object... data)
     {
-        scale = 7.5F;
-        GlStateManager.translate(8F + x, -1.77F + y, -4F);
-        GlStateManager.rotate(0F, 1.0F, 0.0F, 0.0F);
-        GlStateManager.scale(7.5F, 7.5F, 7.5F);
-        GlStateManager.enable(GL11.GL_DEPTH_TEST);
-        GlStateManager.enable(GL11.GL_ALPHA_TEST);
-        GlStateManager.enable(GL11.GL_BLEND);
-        GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        this.renderCachedModel();
+//        scale = 7.5F;
+//        GlStateManager.translate(8F + x, -1.77F + y, -4F);
+//        GlStateManager.rotate(0F, 1.0F, 0.0F, 0.0F);
+//        GlStateManager.scale(7.5F, 7.5F, 7.5F);
+//        GlStateManager.enable(GL11.GL_DEPTH_TEST);
+//        GlStateManager.enable(GL11.GL_ALPHA_TEST);
+//        GlStateManager.enable(GL11.GL_BLEND);
+//        GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+//        this.renderCachedModel();
     }
 
     @Override
     public void renderInWorld(ItemStack item, Object... data)
     {
-        GlStateManager.rotate(180F, 0.0F, 0.0F, 1F);
-        GlStateManager.rotate(90F, 0.0F, 1F, 0F);
-
-        if (entityClass.getSuperclass() == EntityXenomorph.class || entityClass == EntityYautja.class || entityClass == EntityMarine.class)
-        {
-            GlStateManager.scale(0.5F, 0.5F, 0.5F);
-            GlStateManager.translate(0F, -0.5F, -0.5F);
-        }
-        else if (entityClass.getSuperclass() == EntityChestburster.class)
-        {
-            GlStateManager.scale(0.5F, 0.5F, 0.5F);
-            GlStateManager.translate(0F, -1.5F, -0.5F);
-        }
-        else
-        {
-            GlStateManager.scale(1F, 1F, 1F);
-            GlStateManager.translate(0F, -1.25F, 0F);
-        }
-
-        this.renderCachedModel();
+//        GlStateManager.rotate(180F, 0.0F, 0.0F, 1F);
+//        GlStateManager.rotate(90F, 0.0F, 1F, 0F);
+//
+//        if (entityClass.getSuperclass() == EntityXenomorph.class || entityClass == EntityYautja.class || entityClass == EntityMarine.class)
+//        {
+//            GlStateManager.scale(0.5F, 0.5F, 0.5F);
+//            GlStateManager.translate(0F, -0.5F, -0.5F);
+//        }
+//        else if (entityClass.getSuperclass() == EntityChestburster.class)
+//        {
+//            GlStateManager.scale(0.5F, 0.5F, 0.5F);
+//            GlStateManager.translate(0F, -1.5F, -0.5F);
+//        }
+//        else
+//        {
+//            GlStateManager.scale(1F, 1F, 1F);
+//            GlStateManager.translate(0F, -1.25F, 0F);
+//        }
+//
+//        this.renderCachedModel();
     }
 }
